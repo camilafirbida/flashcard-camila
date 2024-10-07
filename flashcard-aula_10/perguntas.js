89dea0c0a6c3905c06fe0a5dc7da6a43b500ce0e
camilafirbida <143540833+camilafirbida@users.noreply.github.com>
2024-10-07 08:17:46 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz casa em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
