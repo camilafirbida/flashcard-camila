1273c1ea931f7fdf8469c9446763136b0885572b
camilafirbida <143540833+camilafirbida@users.noreply.github.com>
2024-10-09 11:08:37 -0300

    Add files via upload

---
+++ flashcard-camila-main/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programação que permite a você implementar itens complexos em páginas web '
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital dos Estados Unidos é Washington'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz amor em Inglês?',
+    'Amor em ingles é love (LOVE)'
+)
